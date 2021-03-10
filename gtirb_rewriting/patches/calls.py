aa4c793d0be26fd182e9e10fff1f1de28aa6271d
Bill LeVine <blevine@grammatech.com>
2021-03-10 10:01:39 -0500

    Fix stack padding for calls

--- gtirb_rewriting/patches/calls.py
+++ gtirb_rewriting/patches/calls.py
@@ -116,7 +116,9 @@ class CallPatch(Patch):
         stack_reg = self._isa.stack_register()
 
         stack_size = sum(stack_slot_size for arg in self._args if not arg.reg)
-        stack_padding = stack_size % self._cconv.stack_alignment
+        stack_padding = self._cconv.stack_alignment - (
+            stack_size % self._cconv.stack_alignment
+        )
         if stack_padding:
             lines.append(f"sub {stack_reg}, {stack_padding}")
 
