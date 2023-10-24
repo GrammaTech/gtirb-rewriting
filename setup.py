049d2ed70e9cfbe92f22253a80b99c630efb3839
Joe Ranieri <jranieri@grammatech.com>
2023-10-24 11:00:18 -0400

    Require mc-asm 0.3.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
         "gtirb-capstone >= 1.0.1",
         "gtirb-functions",
         "gtirb >= 1.10.6",
-        "mcasm ~= 0.2.1",
+        "mcasm ~= 0.3.0",
         "more-itertools",
         "entrypoints",
         "typing-extensions ~= 4.0",
